4b10e3145cb5207505ec907555b4943d46cbd220
Wilmer-44 <gonzalezwilmer0444@gmail.com>
2026-09-06 00:47:51 -0600

    Agregadas variables a Producto.java

--- src/main/java/com/grupo4/semana8/Producto.java
+++ src/main/java/com/grupo4/semana8/Producto.java
@@ -3,4 +3,7 @@ package com.grupo4.semana8;
 public class Producto {
     // TODO: Integrante 2 debe completar la clase Producto.
     private String nombre;
+    private int edad;
+    private String apellido;
+    
 }
