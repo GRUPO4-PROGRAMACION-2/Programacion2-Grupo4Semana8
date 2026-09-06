042756214b4334d9200a85948f4d94a5be1301fc
ellie-24 <elimargm@gmail.com>
2026-09-05 21:52:53 -0600

    Resolucion de conflicto

--- src/main/java/com/grupo4/semana8/Cliente.java
+++ src/main/java/com/grupo4/semana8/Cliente.java
@@ -3,19 +3,18 @@ package com.grupo4.semana8;
 public class Cliente {
     
     private String nombre;
+    private String apellido;
 
-    public Cliente(String nombre) {
+    public Cliente(String nombre, String apellido) {
         this.nombre = nombre;
+        this.apellido = apellido;
     }
 
     public String getNombre() {
         return nombre;
     }
-    private String apellido;
 
-    public Cliente (String apellido ){
-        this.apellido = apellido;
-    }
+   
 }
 
 
