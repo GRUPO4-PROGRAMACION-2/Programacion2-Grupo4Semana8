fee85d9c4cff2f0d79121a41307054b6efbdab4b
ellie-24 <elimargm@gmail.com>
2026-09-05 21:27:05 -0600

    Cambio clase cliente

--- src/main/java/com/grupo4/semana8/Cliente.java
+++ src/main/java/com/grupo4/semana8/Cliente.java
@@ -1,6 +1,12 @@
 package com.grupo4.semana8;
 
 public class Cliente {
-    // TODO: Integrante 1 debe completar la clase Cliente.
     private String nombre;
+    private String apellido;
+
+    public Cliente (String apellido ){
+        this.apellido = apellido;
+    }
 }
+
+
